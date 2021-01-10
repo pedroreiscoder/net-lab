e27a1cefc23f00fd768bb3caf0159f353d18035f
pedroreis <pedroreiscoder@gmail.com>
2021-01-10 10:08:01 -0300

    Adicionado projeto de unit test

---
+++ TesteImposto/Imposto.Core.Tests/NotaFiscalService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Imposto.Core.Tests
+{
+    [TestClass]
+    public class NotaFiscalService
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
