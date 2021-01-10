7cc79090f2ff214074b2b40d89308ae382fab349
pedroreis <pedroreiscoder@gmail.com>
2021-01-10 09:38:40 -0300

    Removendo usings desnecessarios

--- TesteImposto/TesteImposto/FormImposto.cs
+++ TesteImposto/TesteImposto/FormImposto.cs
@@ -1,12 +1,8 @@
 ﻿using Imposto.Core.Service;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Imposto.Core.Domain;
 
--- TesteImposto/TesteImposto/Program.cs
+++ TesteImposto/TesteImposto/Program.cs
@@ -2,9 +2,6 @@
 using Imposto.Core.Service;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TesteImposto
